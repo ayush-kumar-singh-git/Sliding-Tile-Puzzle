b772216ac4296b5bc42f604942ce75e8f4315949
Ayush Kumar Singh <ayushkumares@gmail.com>
2026-03-21 20:03:26 +0530

    random games simulations

--- randomGameSimul.py
+++ randomGameSimul.py
@@ -5,16 +5,20 @@ n = 3
 def generateBoard() :
     board = []
     options = []
+    empty = []
     for i in range(n**2) :
         options.append(i)
     for i in range(n) :
         temp = []
         for j in range(n) :
             option = random.choice(options)
+            if option == 0 :
+                empty.append(i)
+                empty.append(j)
             temp.append(option)
             options.remove(option)
         board.append(temp)
-    return board
+    return board, empty
 
 def evaluate(board) :
     eval = 0
@@ -36,30 +40,30 @@ def printBoard(board):
                 print(f"{val:^3}", end=" ")
         print()  
 
-def getMoves(board) :
+def getMoves(board, i ,j) :
     dx = [1, 0, -1, 0]
     dy = [0, 1, 0, -1]
     moves = []
-    for i in range(n) :
-        for j in range(n) :
-            if board[i][j] == 0 :
-                for k in range(4) :
-                    x = i + dx[k]
-                    y = j + dy[k]
-                    if x >= 0 and x < n and y >= 0 and y < n :
-                        moves.append([x,y])
+    for k in range(4) :
+        x = i + dx[k]
+        y = j + dy[k]
+        if x >= 0 and x < n and y >= 0 and y < n :
+            moves.append([x,y])
     return moves
 
-# board = generateBoard()
-# printBoard(board)
-# print(evaluate(board))
-# moves = getMoves(board)
-# print(moves)
-
-freq = {}
-for i in range (100000) :
-    board = generateBoard()
-    val = evaluate(board)
-    freq[val] = freq.get(val, 0) + 1
-for key in sorted(freq):
-    print(f"{key} : {freq[key]}")
+def playRandomGame()  :
+    board, empty = generateBoard()
+    printBoard(board)
+    print()
+    cnt = 0
+    while(evaluate(board) != 0) :
+        cnt += 1
+        moves = getMoves(board, empty[0], empty[1])
+        move = random.choice(moves)
+        board[empty[0]][empty[1]] = board[move[0]][move[1]]
+        board[move[0]][move[1]] = 0
+        empty = move
+        printBoard(board)
+        print()
+    print(f"Moves : {cnt}")
+playRandomGame()
